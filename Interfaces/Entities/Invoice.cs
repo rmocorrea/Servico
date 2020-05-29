aca40a5b58f23e404ae144ab7ec1e1eedbf7bd3d
Romulo <ro.correabrg@gmail.com>
2020-05-29 18:27:21 -0300

    Serico

---
+++ Interfaces/Entities/Invoice.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace Interfaces.Entities
+{
+    class Invoice
+    {
+        public double BasicPayment { get; set; }
+        public double Tax { get; set; }
+
+        public Invoice(double rental, double tax)
+        {
+            BasicPayment = rental;
+            Tax = tax;
+        }
+
+        public double TotalPayment
+        {
+            get { return BasicPayment + Tax; }
+        }
+
+        public override string ToString()
+        {
+            return "Basic payment: "
+                + BasicPayment.ToString("F2", CultureInfo.InvariantCulture)
+                + "\nTax: "
+                + Tax.ToString("F2", CultureInfo.InvariantCulture)
+                + "\nTotal payment: "
+                + TotalPayment.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Interfaces/Service/BrazilTaxService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Interfaces.Entities;
+
+namespace Service
+{
+    class BrazilTaxService : ITaxService
+    {
+        public double Tax(double amount)
+        {
+            if (amount <= 100.00)
+            {
+                return amount * 0.2;
+            }
+            else
+            {
+                return amount * 0.15;
+            }
+        }
+    }
+}
